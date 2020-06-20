d805e7717d42a917883fe0fe49c73de8edf27039
Jean-Maurice Boussonville <muecke95@web.de>
2020-06-20 02:19:17 +0200

    BPM Implementierung

--- Tracker5.0 WatchKit Extension/secondInterface_Controller.swift
+++ Tracker5.0 WatchKit Extension/secondInterface_Controller.swift
@@ -12,11 +12,15 @@ import HealthKit
 
 
 
+
 class secondInterface_Controller: WKInterfaceController {
 
     private var auswahl: String = "leer"
     
+    private var value: Int = 0
     
+    public let healthStore = HKHealthStore()
+    let heartRateQuantity = HKUnit(from: "count/min")
     
     
     override func awake(withContext context: Any?) {
@@ -34,14 +38,62 @@ class secondInterface_Controller: WKInterfaceController {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
+    
+    private func startHeartRateQuery(quantityTypeIdentifier: HKQuantityTypeIdentifier) {
+        
+        // We want data points from our current device
+        let devicePredicate = HKQuery.predicateForObjects(from: [HKDevice.local()])
+        
+        // A query that returns changes to the HealthKit store, including a snapshot of new changes and continuous monitoring as a long-running query.
+        let updateHandler: (HKAnchoredObjectQuery, [HKSample]?, [HKDeletedObject]?, HKQueryAnchor?, Error?) -> Void = {
+            query, samples, deletedObjects, queryAnchor, error in
+            
+         // A sample that represents a quantity, including the value and the units.
+        guard let samples = samples as? [HKQuantitySample] else {
+            return
+        }
+            
+        self.process(samples, type: quantityTypeIdentifier)
+
+        }
+        
+        // It provides us with both the ability to receive a snapshot of data, and then on subsequent calls, a snapshot of what has changed.
+        let query = HKAnchoredObjectQuery(type: HKObjectType.quantityType(forIdentifier: quantityTypeIdentifier)!, predicate: devicePredicate, anchor: nil, limit: HKObjectQueryNoLimit, resultsHandler: updateHandler)
+        
+        query.updateHandler = updateHandler
+        
+        // query execution
+        
+        healthStore.execute(query)
+    }
+    
+    private func process(_ samples: [HKQuantitySample], type: HKQuantityTypeIdentifier) {
+        var lastHeartRate = 0.0
+        
+        for sample in samples {
+            if type == .heartRate {
+                lastHeartRate = sample.quantity.doubleValue(for: heartRateQuantity)
+            }
+            
+            self.value = Int(lastHeartRate)
+        }
+    }
+    
+    func blah(){
+    }
+    
+    @IBOutlet var label: WKInterfaceLabel!
 
     @IBOutlet var myPicker: WKInterfacePicker!
     
-    @IBAction func pickerAction(_ value: Int){
-        print("Es wurde gewählt: \(value)")
+    @IBAction func pickerAction(_ wahl: Int){
+        print("Es wurde gewählt: \(wahl)")
+        startHeartRateQuery(quantityTypeIdentifier: .heartRate)
+        self.label.setText("\(value)")
+        print("\(value)")
         let array: [WKPickerItem] = setPickerItems()
-        if (array[value].title != nil) {
-            auswahl = array[value].title ?? "leer"
+        if (array[wahl].title != nil) {
+            auswahl = array[wahl].title ?? "leer"
         }
     }
     
@@ -52,31 +104,31 @@ class secondInterface_Controller: WKInterfaceController {
         pushController(withName: "thirdinterface", context: nil)
     }
     
-        override init(){
-                super.init()
-                myPicker.setItems(setPickerItems())
-                myPicker.focus()
-            }
+    override init(){
+        super.init()
+        myPicker.setItems(setPickerItems())
+        myPicker.focus()
+    }
     
-            func setPickerItems() -> [WKPickerItem] {
-                let sehrSchwach = pickerItemWithTitle("Sehr Schwach")
-                sehrSchwach.caption = "Wie Stark?"
-                let schwach = pickerItemWithTitle("Schwach")
-                schwach.caption = "Wie Stark?"
-                let  mittel = pickerItemWithTitle("Mittel")
-                mittel.caption = "Wie Stark?"
-                let stark = pickerItemWithTitle("Stark")
-                stark.caption = "Wie Stark?"
-                let sehrStark = pickerItemWithTitle("Sehr Stark")
-                sehrStark.caption = "Wie Stark?"
-                return [sehrSchwach, schwach, mittel, stark, sehrStark]
-            }
+    func setPickerItems() -> [WKPickerItem] {
+        let sehrSchwach = pickerItemWithTitle("Sehr Schwach")
+        sehrSchwach.caption = "Wie Stark?"
+        let schwach = pickerItemWithTitle("Schwach")
+        schwach.caption = "Wie Stark?"
+        let  mittel = pickerItemWithTitle("Mittel")
+        mittel.caption = "Wie Stark?"
+        let stark = pickerItemWithTitle("Stark")
+        stark.caption = "Wie Stark?"
+        let sehrStark = pickerItemWithTitle("Sehr Stark")
+        sehrStark.caption = "Wie Stark?"
+        return [sehrSchwach, schwach, mittel, stark, sehrStark]
+    }
     
-            func pickerItemWithTitle(_ title: String) -> WKPickerItem {
-                let pickerItem = WKPickerItem()
-                pickerItem.title = title
-                return pickerItem
-            }
+    func pickerItemWithTitle(_ title: String) -> WKPickerItem {
+        let pickerItem = WKPickerItem()
+        pickerItem.title = title
+        return pickerItem
+    }
     
     
     // MARK: - Health
